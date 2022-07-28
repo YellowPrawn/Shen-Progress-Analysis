de0a770b6d511d3a4329a2b60d2ff1bab7347354
Damien Fung <damienfung616@gmail.com>
2022-07-28 16:07:05 +0800

    turned summary function into script.

--- src/get_shroom_summary.py
+++ src/get_shroom_summary.py
@@ -4,34 +4,58 @@ import get_participantId as gp
 import get_shrooms_created as gsc
 import get_shrooms_destroyed as gsde
 import get_shroom_damage as gsd
+import pandas as pd
+import numpy as np
+import argparse
 
 #' Collects timeline from a given teemo match for a given player
 #' 
 #' Pulls match timeline from Riot API and extracts number of shrooms placed and destroyed
-#' this function is not feasibly testable as player match history is not static
+#' this script is not feasibly testable as player match history is not static
 #' 
 #' @param my_region region of player
 #' @param summoner_name name of player
 #' @param matches number of matches to scrape 
-#' @param api_key api key to pull off the riot API
 #'
 #' @return a number of shrooms placed, number of shrooms destroyed
 #'
 #' @export
 #'
 #' @examples
-#' get_shroom_summary('na1', 'PrawnJ', 1, 'RGAPI-xxxxx')
-def get_shroom_summary(my_region, summoner_name, matches, api_key):
-    watcher = LolWatcher(api_key)
-    teemo_matches, puuid = gm.get_match(my_region, summoner_name, 'Teemo', matches, watcher)
-    
-    for m in teemo_matches:
-        timeline = watcher.match.timeline_by_match(my_region, m)
-        participant_id = gp.get_participantId(my_region, puuid, m, watcher)
+#' python ./src/get_shroom_summary.py "na1" "PrawnJ" 6 
+parser = argparse.ArgumentParser(description='Makes summary table of teemo shroom data')
+parser.add_argument('region', metavar='source', type=str, help='region of player')
+parser.add_argument('summoner_name', metavar='path', type=str, help='username of player')
+parser.add_argument('matches', metavar='path', type=int, help='number of matches to be scraped')
 
-        for f in timeline['info']['frames']:
-            shrooms_created = gsc.get_shrooms_created(participant_id, f)
-            shrooms_destroyed = gsde.get_shroom_destroyed(f)
-            shroom_damage = gsd.get_shroom_damage(f)
-            #TODO: add this data into numpy table
+args = parser.parse_args()
+my_region = args.region
+summoner_name = args.summoner_name
+matches = args.matches
+
+api_key = open("api_key.txt", "r").readline()
+watcher = LolWatcher(api_key)
+teemo_matches, puuid = gm.get_match(my_region, summoner_name, 'Teemo', matches, watcher)
+    
+summary_table = pd.DataFrame(columns=['matchId','shrooms_created','shrooms_destroyed','shroom_damage'])
+
+for m in teemo_matches:
+    timeline = watcher.match.timeline_by_match(my_region, m)
+    participant_id = gp.get_participantId(my_region, puuid, m, watcher)
+
+    shrooms_created = 0
+    shrooms_destroyed = 0 
+    shroom_damage = 0
+        
+    for f in timeline['info']['frames']:
+        shrooms_created = shrooms_created + gsc.get_shrooms_created(participant_id, f)
+        shrooms_destroyed = shrooms_destroyed + gsde.get_shrooms_destroyed(f)
+        shroom_damage = shroom_damage + gsd.get_shroom_damage(f)
+    
+    match_summary = pd.DataFrame({'matchId': [m], 
+                                'shrooms_created': [shrooms_created], 
+                                'shrooms_destroyed': [shrooms_destroyed], 
+                                'shroom_damage': [shroom_damage]})
+    summary_table = pd.concat([summary_table, match_summary])
+summary_table.to_csv('./data/shroom_summary.csv', index = None, header=True)
         
